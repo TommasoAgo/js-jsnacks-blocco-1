1a004fa3b5157fa5f910542d8b8e1149c811e4d3
Tommaso Agostini <tommasoagostini90@gmail.com>
2021-04-13 13:54:12 +0200

    Fine seconda parte

--- Zucchini2/js/script.js
+++ Zucchini2/js/script.js
@@ -1,5 +1,6 @@
-// Crea 10 oggetti che rappresentano una zucchina, indicandone per ognuno varietà, peso e lunghezza. 
-// Calcola la somma del peso di tutte le zucchine.
+// Crea 10 oggetti che rappresentano una zucchina.
+// Dividi in due array separati le zucchine che misurano meno o più di 15cm. 
+// Infine stampa separatamente quanto pesano i due gruppi di zucchine.
 
 // Creo l'Array che contengono gli oggetti 'Zucchine'
 var arrayZucchine = [
@@ -55,19 +56,23 @@ var arrayZucchine = [
     }
 ]
 
-// Imposto il peso totale a 0 per sommare il peso di ogni singolo zucchino in un loop
-var pesoTotale = 0;
+// Creo due Array vuoti per pusharci gli zucchini in base alle lunghezze
+var zucchineCorte = [];
+var zucchineLunghe = [];
 
-// Faccio un loop for per prendere il peso di ogni singolo zucchino e sommarlo a pesoTotale
+// Creo una variabile per includere il valore che volgio comparare
+var valoreSeparatore = 15;
+
+
+// Creo un loop for per prendere uno a uno le lunghezze degli zucchini
 for ( var i = 0; i < arrayZucchine.length; i++ ) {
     var thisZucchino = arrayZucchine[i];
-    pesoTotale += thisZucchino.peso;
+    if ( thisZucchino.lunghezza > valoreSeparatore ) {
+        zucchineLunghe.push(thisZucchino);
+    } else if ( thisZucchino.lunghezza < valoreSeparatore ) {
+        zucchineCorte.push(thisZucchino);
+    }
 }
 
-// Stampo il peso totale
-console.log(pesoTotale);
-
-
-// Crea 10 oggetti che rappresentano una zucchina.
-// Dividi in due array separati le zucchine che misurano meno o più di 15cm. 
-// Infine stampa separatamente quanto pesano i due gruppi di zucchine.
+console.log(zucchineCorte);
+console.log(zucchineLunghe);
