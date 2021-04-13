545aa3cfef0aab835c053ff190d6fdab4c7f6d64
Tommaso Agostini <tommasoagostini90@gmail.com>
2021-04-13 13:30:51 +0200

    Creata Repo

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Crea 10 oggetti che rappresentano una zucchina, indicandone per ognuno varietà, peso e lunghezza. 
+// Calcola la somma del peso di tutte le zucchine.
+
+// Crea 10 oggetti che rappresentano una zucchina.
+// Dividi in due array separati le zucchine che misurano meno o più di 15cm. 
+// Infine stampa separatamente quanto pesano i due gruppi di zucchine.
+
